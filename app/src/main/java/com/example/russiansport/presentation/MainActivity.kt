7b82ea48441bf4d457ff05bbc1a893df3908bca2
grigorijbusahin <gbusaxin@gmail.com>
2022-01-10 21:12:03 +0200

    ChromeClient and save data when orientation is changed

--- app/src/main/java/com/example/russiansport/presentation/MainActivity.kt
+++ app/src/main/java/com/example/russiansport/presentation/MainActivity.kt
@@ -4,20 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.res.Resources
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.net.Uri
+import android.os.*
 import android.util.Log
-import android.view.View
-import android.webkit.WebViewClient
-import android.widget.Toast
+import android.view.KeyEvent
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.ConfigurationCompat
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
-import com.example.russiansport.R
 import com.example.russiansport.data.network.dto.RequestDto
 import com.example.russiansport.data.network.dto.ResponseDto
 import com.example.russiansport.data.network.retrofit.ApiFactory
@@ -25,11 +22,35 @@ import com.example.russiansport.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import android.webkit.ValueCallback
+import android.widget.Toast
+
+import android.content.ActivityNotFoundException
+
+import android.webkit.WebView
+
+import android.os.Build
+
+import android.annotation.TargetApi
+import android.webkit.WebChromeClient.FileChooserParams
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val apiService = ApiFactory.apiService
+    private lateinit var webView: WebView
+
+    private val INPUT_FILE_REQUEST_CODE = 1
+    private val FILECHOOSER_RESULTCODE = 1
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var mUploadMessage: ValueCallback<Uri>
+    private lateinit var mFilePathCallback: ValueCallback<Array<Uri?>?>
+
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,12 +59,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         loadJsonPicture()
 
-        val webView = binding.webView
+        webView = binding.webView
         val refreshLay = binding.refreshLayout
         val progressBar = binding.progressBar
 
         val settings = webView.settings
 
+        applyViewSettingsForWebView(settings)
         val locale = ConfigurationCompat.getLocales(Resources.getSystem().configuration)
         val lang = locale[0]
         Log.d("LANGUAGE_CURR", lang.isO3Language)
@@ -52,39 +74,59 @@ class MainActivity : AppCompatActivity() {
         val request = responseDto.enqueue(object : Callback<ResponseDto> {
             @SuppressLint("SetJavaScriptEnabled")
             override fun onResponse(p0: Call<ResponseDto>, p1: Response<ResponseDto>) {
-                Log.d("P1BODY","Ответ - ${p1.body().toString()}")
-                if(p1.body()?.response == "no"){
-                    startActivity(Intent(this@MainActivity,MenuActivity::class.java))
-                }else{
+                Log.d("P1BODY", "Ответ - ${p1.body().toString()}")
+                if (p1.body()?.response == "no") {
+                    startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+                } else {
                     supportActionBar?.hide()
-                    with(settings){
-                        javaScriptEnabled = true
-                        domStorageEnabled = true
-                    }
-                    webView.webViewClient = CustomWebClient(applicationContext,progressBar)
-                    webView.loadUrl(p1.body()?.response?:"")
+                    webView.webViewClient = CustomWebClient(applicationContext, progressBar)
+                    webView.webChromeClient = CustomChromeClient()
+                    webView.loadUrl(p1.body()?.response ?: "")
                 }
             }
 
             override fun onFailure(p0: Call<ResponseDto>, p1: Throwable) {
-                startActivity(Intent(this@MainActivity,MenuActivity::class.java))
-                Log.d("ONFAILURE",p1.message.toString())
+                startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+                Log.d("ONFAILURE", p1.message.toString())
             }
         })
 
-        refreshLay.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener{
+        // обновление страницы
+        refreshLay.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
             override fun onRefresh() {
                 refreshLay.isRefreshing = true
                 Handler(Looper.getMainLooper()).postDelayed({
                     refreshLay.isRefreshing = false
                     webView.loadUrl(webView.url.toString())
-                },2500)
+                }, 1000)
             }
 
         })
 
     }
 
+    //нажатие назад к вебклиенту или приложению
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    //настройки webview
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun applyViewSettingsForWebView(settings: WebSettings) {
+        settings.domStorageEnabled = true
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.displayZoomControls = false
+        settings.allowFileAccess = true
+        settings.allowContentAccess = true
+        settings.javaScriptEnabled = true
+    }
+
+    //бэкграунд
     private fun loadJsonPicture() {
         try {
             Glide.with(this).load("http://95.217.132.144/russian_sport/background_image.jpg")
@@ -98,6 +140,44 @@ class MainActivity : AppCompatActivity() {
                     }
                 })
         } catch (e: Exception) {
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        mFilePathCallback.onReceiveValue(Array())
+    }
+
+    inner class CustomChromeClient : WebChromeClient() {
+
+        override fun onPermissionRequest(request: PermissionRequest?) {
+            request?.grant(request.resources)
+        }
+
+        fun openFileChooser(uploadMsg: ValueCallback<Uri>, acceptType: String?) {
+            mUploadMessage = uploadMsg
+            val i = Intent(Intent.ACTION_GET_CONTENT)
+            i.addCategory(Intent.CATEGORY_OPENABLE)
+            i.type = "image/*"
+            startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE)
+        }
+
+        override fun onShowFileChooser(
+            vw: WebView?, filePathCallback: ValueCallback<Array<Uri?>?>,
+            fileChooserParams: FileChooserParams?
+        ): Boolean {
+            if (mFilePathCallback != null) {
+                mFilePathCallback.onReceiveValue(null)
+            }
+            mFilePathCallback = filePathCallback
+            val selectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+            selectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+            selectionIntent.type = "*/*"
+            val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+            chooserIntent.putExtra(Intent.EXTRA_INTENT, selectionIntent)
+            startActivityForResult(chooserIntent, 0)
+            return true
         }
     }
 }
